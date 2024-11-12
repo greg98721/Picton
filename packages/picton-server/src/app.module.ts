97cb30a3cd0a2257c858098dd443b19e1edf7059
Greg Ede <gregory.ede@gmail.com>
2024-11-12 22:53:20 +0000

    Updated app module to copy items from blenheim

--- packages/picton-server/src/app.module.ts
+++ packages/picton-server/src/app.module.ts
@@ -1,10 +1,80 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import * as Joi from 'joi';
+
+import { join } from 'path';
+
+import { AirRoutesController } from './air-routes/air-routes.controller';
+import { FlightsController } from './flights/flights.controller';
+import { AuthController } from './auth/auth.controller';
+import { UsersController } from './users/users.controller';
+import { BookingsController } from './bookings/bookings.controller';
+
+import { ScheduleService } from './schedule/schedule.service';
+import { UsersService } from './users/users.service';
+import { AuthService } from './auth/auth.service';
+import { BookingsService } from './bookings/bookings.service';
+import { FrontendMiddleware } from './FrontendMiddleware';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    // This goes first so that the config service is available to other modules
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        CLIENT_DIST: Joi.string().required(),
+        CLIENT_INDEX_FILE: Joi.string().required(),
+        ACCESS_TOKEN_SECRET: Joi.string().required(),
+        ACCESS_TOKEN_EXPIRATION: Joi.number().required(),
+        REFRESH_TOKEN_SECRET: Joi.string().required(),
+        REFRESH_TOKEN_EXPIRATION: Joi.number().required(),
+      }),
+    }),
+    // This enables the server to serve the client files from the dist folder
+    // Noye we use the async version of forRoot so that we can inject the config service
+    ServeStaticModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => [
+        {
+          rootPath: join(
+            __dirname,
+            configService.get<string>('CLIENT_DIST') || '',
+          ),
+        },
+      ],
+    }),
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('ACCESS_TOKEN_SECRET'),
+        signOptions: {
+          expiresIn: configService.get<number>('ACCESS_TOKEN_EXPIRATION'),
+        },
+      }),
+    }),
+  ],
+  controllers: [
+    AirRoutesController,
+    FlightsController,
+    AuthController,
+    UsersController,
+    BookingsController,
+  ],
+  providers: [ScheduleService, UsersService, AuthService, BookingsService],
 })
-export class AppModule {}
+// To enable the server to host the client files
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): void {
+    consumer.apply(FrontendMiddleware).forRoutes({
+      path: '/**', // For all routes
+      method: RequestMethod.ALL, // For all methods
+    });
+  }
+}
